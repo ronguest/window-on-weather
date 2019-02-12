52627e6b6339c436d9a47b30c97f150784098979
ronguest <ronguest@protonmail.com>
2019-02-12 12:18:34 -0600

    Modified video.py to record a throw-away video at the start to let auto-exposure settle before recording the main video

--- video.py
+++ video.py
@@ -3,10 +3,13 @@ import picamera
 
 camera = picamera.PiCamera()
 camera.resolution = (1280, 720)
+camera.framerate = 15
+# First few seconds are blown out as auto-exposure kicks in, so record a dummy first.
+camera.start_recording('trash.h264')
+camera.wait_recording(5)
+camera.stop_recording()
+
 camera.framerate = 2
-camera.start_preview()
-# Camera warm-up time
-sleep(5)
 camera.start_recording('test.h264')
 camera.wait_recording(140)
 camera.stop_recording()
