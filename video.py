a90a379ffca733a27323c9c4556bdd1f8faa0219
ronguest <ronguest@protonmail.com>
2019-02-13 16:20:38 -0600

    Reduced mp4 resolution for smaller files

--- video.py
+++ video.py
@@ -2,7 +2,8 @@ from time import sleep
 import picamera
 
 camera = picamera.PiCamera()
-camera.resolution = (1280, 720)
+#camera.resolution = (1280, 720)
+camera.resolution = (960, 540)
 camera.framerate = 15
 # First few seconds are blown out as auto-exposure kicks in, so record a dummy first.
 camera.start_recording('trash.h264')
